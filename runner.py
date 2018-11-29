564a18151186c83029a8e4b9f176e494ecd6e3d9
DavidAwogbemila <david.awogbemila@bison.howard.edu>
2018-11-29 11:33:01 -0500

    Need to get url parameters right.

--- runner.py
+++ runner.py
@@ -2,9 +2,19 @@
 Filename: runner.py
 Authors: Ikechuckwu A., David A
 """
+import pandas as pd
+
 api_key = "14B99EBD-FB07-36A1-A02F-3B0322779DCA"
 
+def get_data(src=""):
+  return pd.read_csv(src)
+
 def main():
+  try:
+    src = "http://quickstats.nass.usda.gov/api/api_GET/?key="+ api_key +"&year__GE=1989&year__LE=2018&program=SURVEY&sector='ANIMALS & PRODUCTS'&group=POULTRY&commodity=POULTRY&category=SLAUGHTERED&data_item__LIKE='F1 - SLAUGHTERED, MEASURED IN HEAD'&format=csv"
+    data = pd.read_csv(src, nrows=100)
+  except Exception as e:
+    print("Error " + str(e))
   pass
 
 if __name__ == "__main__":
