3299f6608e3268474c491f6223f7e94b19ac775d
DavidAwogbemila <david.awogbemila@bison.howard.edu>
2018-11-29 20:51:17 -0500

    Determine whether to load file locally or via http.

--- runner.py
+++ runner.py
@@ -2,25 +2,29 @@
 Filename: runner.py
 Authors: Ikechuckwu A., David A
 """
+import os
 import pandas as pd
 
 api_key = "14B99EBD-FB07-36A1-A02F-3B0322779DCA"
-
-def get_data(src=""):
-  return pd.read_csv(src)
+raw_data_filename = "raw_data.csv"
 
 def main():
+  file_exists = True
   try:
-    src = "http://quickstats.nass.usda.gov/api/api_GET/?key="+ api_key +"&year__GE=1989&year__LE=2018&program=SURVEY&sector=\'ANIMALS & PRODUCTS\'&group=POULTRY&commodity_desc=TURKEYS&category=SLAUGHTERED&data_item__LIKE='F1 - SLAUGHTERED, MEASURED IN HEAD'&format=csv&state_alpha=VA"
-    data = pd.read_csv(src, nrows=100)
-    print(data.columns)
-    print(data.shape)
-    print(data['load_time'].describe())
-    #http://quickstats.nass.usda.gov/api/api_GET/?key=14B99EBD-FB07-36A1-A02F-3B0322779DCA&year__GE=1989&year__LE=2018&program=SURVEY&sector=\'ANIMALS & PRODUCTS\'&group=POULTRY&commodity=TURKEYS&category=SLAUGHTERED&data_item__LIKE='F1 - SLAUGHTERED, MEASURED IN HEAD'&format=csv&state=VA
-    print(data.head())
+    if not os.path.isfile(raw_data_filename):
+      print("File [" + raw_data_filename + "] not present. Fetching via http...")
+      src = "http://quickstats.nass.usda.gov/api/api_GET/?key="+ api_key +"&year__GE=1989&year__LE=2018&program=SURVEY&sector=\'ANIMALS & PRODUCTS\'&group=POULTRY&commodity_desc=TURKEYS&category=SLAUGHTERED&data_item__LIKE='F1 - SLAUGHTERED, MEASURED IN HEAD'&format=csv&state_alpha=VA"
+      file_exists = False
+    else:
+      print("File [" + raw_data_filename + "] exists. Fetching locally...")
+      src = raw_data_filename
+    data = pd.read_csv(src)
+    print("Finished fetching. Data size: [" + str(data.shape[0]) +" x " + str(data.shape[1]) + "]")
+    if not file_exists:
+      data.to_csv(raw_data_filename)
+
   except Exception as e:
     print("Error " + str(e))
-  pass
 
 if __name__ == "__main__":
   main()
