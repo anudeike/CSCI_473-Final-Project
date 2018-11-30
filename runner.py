94a0e87bee473bb31eb9e477c4f20a80d1831135
DavidAwogbemila <david.awogbemila@bison.howard.edu>
2018-11-29 20:36:19 -0500

    Gets data [100 x 39] dataframe.

--- runner.py
+++ runner.py
@@ -11,8 +11,13 @@ def get_data(src=""):
 
 def main():
   try:
-    src = "http://quickstats.nass.usda.gov/api/api_GET/?key="+ api_key +"&year__GE=1989&year__LE=2018&program=SURVEY&sector='ANIMALS & PRODUCTS'&group=POULTRY&commodity=POULTRY&category=SLAUGHTERED&data_item__LIKE='F1 - SLAUGHTERED, MEASURED IN HEAD'&format=csv"
+    src = "http://quickstats.nass.usda.gov/api/api_GET/?key="+ api_key +"&year__GE=1989&year__LE=2018&program=SURVEY&sector=\'ANIMALS & PRODUCTS\'&group=POULTRY&commodity_desc=TURKEYS&category=SLAUGHTERED&data_item__LIKE='F1 - SLAUGHTERED, MEASURED IN HEAD'&format=csv&state_alpha=VA"
     data = pd.read_csv(src, nrows=100)
+    print(data.columns)
+    print(data.shape)
+    print(data['load_time'].describe())
+    #http://quickstats.nass.usda.gov/api/api_GET/?key=14B99EBD-FB07-36A1-A02F-3B0322779DCA&year__GE=1989&year__LE=2018&program=SURVEY&sector=\'ANIMALS & PRODUCTS\'&group=POULTRY&commodity=TURKEYS&category=SLAUGHTERED&data_item__LIKE='F1 - SLAUGHTERED, MEASURED IN HEAD'&format=csv&state=VA
+    print(data.head())
   except Exception as e:
     print("Error " + str(e))
   pass
