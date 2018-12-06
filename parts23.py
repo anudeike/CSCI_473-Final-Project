9fbc1b2e68b9a416b453d35a57fdc374b2496442
DavidAwgbemila <david.awogbemila@bison.howard.edu>
2018-12-06 12:39:28 -0500

    Do last part of 2: show mean and medians by year.

--- parts23.py
+++ parts23.py
@@ -14,6 +14,7 @@ def get_data():
   data = pd.read_csv("raw_data.csv")
   return data
 
+# I think this may have been unnecessary, end_code would have sufficed.
 year_index = {
   "JAN": 1,
   "FEB": 2,
@@ -45,21 +46,26 @@ def create_line_plot(data):
   # Period where drop occurs between 2002 and 2009
   # plt.plot(x[150: 175], y[150: 175])
 
-  plt.plot(x, y)
-  plt.ylabel("Value")
-  plt.xlabel("Year-Month")
-  plt.show()
+  # plt.plot(x, y)
+  # plt.ylabel("Value")
+  # plt.xlabel("Year-Month")
+  # plt.show()
 
   # TODO(anude, awogbemila): Report Mean and Median of the Value grouped by year.
+  unique_years = data.year.unique()
+  for year in unique_years:
+    year_data = data.loc[data["year"] == year]
+    year_value = year_data["Value"].apply(lambda val: int(val.replace(",", "")))
+    print("year %s has mean %s and median %s" % (year, year_value.mean(), year_value.median()))
 
 def do_november_2017_regression(data):
   data_for_2017 = data.loc[data["year"] == 2017]
   just_till_october = data_for_2017.loc[lambda df: df["reference_period_desc"] != "NOV"]
   just_till_october = just_till_october.loc[lambda df: df["reference_period_desc"] != "DEC"]
 
-
   x = list(range(1, 11))
   y = just_till_october["Value"].apply(lambda x: int(x.replace(",", "")))
+  y = y.reset_index().drop(["index"], axis=1)
 
   # (a)
   # Assume y = mx + c
@@ -80,18 +86,24 @@ def do_november_2017_regression(data):
   print("Absolute error %s" % abs_error)
 
   # (d)
-  # TODO(anude, awogbemila): compute r-squared value (over what time period)
+  # TODO(anude, awogbemila): compute r-squared value (over what time period?)
+  # I'll assume just 2017.
   x = list(range(1, 11))
+ 
   value_mean = y.mean()
-  predictions = pd.DataFrame([m * i + c for i in x])
+  predictions = pd.DataFrame([m * i + c for i in x], columns=["Value"])
   SSR = sum(((predictions - value_mean).apply(lambda val: val ** 2)).values)[0]
+  SSE = sum((y - predictions).apply(lambda val: val ** 2).values)
   SSTO = sum((y - value_mean).apply(lambda val: val ** 2).values)
-  print(SSR, SSTO)
   r_squared = SSR / SSTO
   print("r-squared", r_squared)
 
+  # Alternate R-Squared method verified to give smae result.
+  # r_squared_ = 1 - (SSE / SSTO)
+  # print("r-squared_", r_squared_)
+
   # Sanity check: gives good result. Don't forget to import scipy.stats
-  # _, _, sprsq, _, _ = stats.linregress(x, y)
+  # _, _, sprsq, _, _ = stats.linregress(x, y["Value"])
   # print("scipy r-squared", r_squared)
 
   # (e)
@@ -102,8 +114,8 @@ def do_november_2017_regression(data):
 
 def main():
   data = get_data()
-  # create_line_plot(data)
-  do_november_2017_regression(data)
+  create_line_plot(data)
+  # do_november_2017_regression(data)
 
 if __name__ == "__main__":
   main()
